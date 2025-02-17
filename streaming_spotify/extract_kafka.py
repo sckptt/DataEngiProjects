ef952b9c8296d19485593238b25bc5ba376a8475
sckptt <kiddovpm@gmail.com>
2025-02-17 14:17:14 +0700

    Connect with Postgre - OK

--- streaming_spotify/extract_kafka.py
+++ streaming_spotify/extract_kafka.py
@@ -1,11 +1,15 @@
 import findspark
 import json
 import os
+import psycopg2
 import redis
 import spotipy
 import time
+from colorama import init
+from termcolor import colored
 from dotenv import load_dotenv
 from kafka import KafkaProducer
+from psycopg2 import OperationalError
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, from_unixtime, year, month, dayofmonth, dayofweek, hour, minute
 from pyspark.sql.types import StructType, StringType, ArrayType, LongType, StructField
@@ -13,6 +17,7 @@ from spotipy.oauth2 import SpotifyOAuth
 
 findspark.init()
 load_dotenv()
+init()
 
 def create_spotify_client():
 	try:
@@ -29,10 +34,10 @@ def create_spotify_client():
 			client_secret=my_secret,
 			redirect_uri=my_redirect_uri,
 			scope=my_scopes))
-		print("Spotify client is created")
+		print(colored("Spotify client is created", "green"))
 		return my_sp
 	except Exception as e:
-		print(f"Couldn't create the spotify client due to exception {e}")
+		print(colored(f"Couldn't create the spotify client due to exception {e}", "red"))
 		return None
 
 def parse_playback(playback : dict) -> dict:
@@ -58,12 +63,13 @@ def parse_playback(playback : dict) -> dict:
 			song_id = playback["item"]["id"]
 			timestamp = playback["timestamp"]
 			playing_type = playback["currently_playing_type"]
+			print(colored("Playback is parsed successfully", "green"))
 			return {
 				"device_name" : device_name,
 				"device_type" : device_type,
 				"url" : url,
 				"artists_name" : artists_name,
-				"artist_id" : artists_id,
+				"artists_id" : artists_id,
 				"album_name" : album_name,
 				"album_picture" : album_picture,
 				"song_name" : song_name,
@@ -72,10 +78,10 @@ def parse_playback(playback : dict) -> dict:
 				"playing_type" : playing_type
 			}
 	except KeyError as e:
-		print(f"Key error occured: {e}")
+		print(colored(f"Key error occured: {e}", "red"))
 		return None
 	except Exception as e:
-		print(f"An error occured while parsing playback: {e}")
+		print(colored(f"An error occured while parsing playback: {e}", "red"))
 		return None
 
 def check_duplicates(pp : dict, redis_client: redis.Redis) -> bool:
@@ -97,15 +103,15 @@ def create_spark_connection():
 			.config("spark.driver.extraJavaOptions", "-Duser.library.path=$JAVA_HOME/lib") \
 			.config("spark.executor.extraJavaOptions", "-Duser.library.path=$JAVA_HOME/lib") \
 			.config("spark.java.options", "-Dfile.encoding=UTF-8") \
-			.config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.4") \
+			.config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0,org.postgresql:postgresql:42.3.8") \
 			.config("spark.ui.enabled", "false") \
 			.getOrCreate()
-		
+
 		spark_connection.sparkContext.setLogLevel("ERROR")
-		print("Spark connection is created")
+		print(colored("Spark connection is created", "green"))
 		return spark_connection
 	except Exception as e:
-		print(f"Couldn't create the spark session due to exception {e}")
+		print(colored(f"Couldn't create the spark session due to exception {e}", "red"))
 		return None
 
 def create_kafka_connection(spark_connection):
@@ -119,10 +125,10 @@ def create_kafka_connection(spark_connection):
 			.option("enable.auto.commit", "true") \
     		.option("group.id", "spotify_consumer_group") \
 			.load()
-		print("Spark connected with Kafka successfully")
+		print(colored("Spark connected with Kafka successfully", "green"))
 		return data_frame
 	except Exception as e:
-		print(f"Couldn't create a dataframe due to exception {e}")
+		print(colored(f"Couldn't create a dataframe due to exception {e}", "red"))
 		return None
 
 def parse_streaming_schema(data_frame):
@@ -144,9 +150,11 @@ def parse_streaming_schema(data_frame):
 			.selectExpr("CAST(value AS STRING) as json_string") \
 			.select(from_json(col("json_string"), schema).alias("data")) \
 			.select("data.*")
+		
+		print(colored("Data frame is parsed successfully", "green"))
 		return parsed_df
 	except Exception as e:
-		print(f"Couldn't parse a data frame due to exception {e}")
+		print(colored(f"Couldn't parse a data frame due to exception {e}", "red"))
 		return None
 
 def add_datetime_columns(data_frame):
@@ -161,10 +169,88 @@ def add_datetime_columns(data_frame):
 		# times_played = enriched_df.groupBy("song_id", "day", "month", "year").agg(count("song_id").alias("times_played"))
 		# deduplicated_df = enriched_df.dropDuplicates(["song_id", "day", "month", "year"])
 		# final_df = deduplicated_df.join(times_played, on=["song_id", "day", "month", "year"])
+		print(colored("Data frame is enriched successfully", "green"))
 		return enriched_df
 	except Exception as e:
-		print(f"Couldn't add datetime columns due to exception {e}")
+		print(colored(f"Couldn't add datetime columns due to exception {e}", "red"))
 		return None
+
+def create_postgre_connection():
+	try:
+		postgre_connection = psycopg2.connect(
+			dbname = os.getenv("DB_NAME"),
+			user = os.getenv("DB_USER"),
+			password = os.getenv("DB_PASSWORD"),
+			host = os.getenv("DB_HOST"),
+			port = os.getenv("DB_PORT"),
+			sslmode="require")
+		
+		print(colored("PostgreSQL connection is created successfully", "green"))
+		return postgre_connection
+	except OperationalError as e:
+		print(colored(f"Couldn't create the PostgreSQL connection due to exception {e}", "red"))
+		return None
+
+def create_table(postgre_connection):
+	try:
+		cursor = postgre_connection.cursor()
+		create_table_query = '''
+			CREATE TABLE IF NOT EXISTS spotify_tracks (
+			id SERIAL PRIMARY KEY,
+			device_name VARCHAR(100),
+			device_type VARCHAR(100),
+			url VARCHAR(200),
+			artists_name TEXT[],
+			artists_id TEXT[],
+			album_name VARCHAR(100),
+			album_picture VARCHAR(200),
+			song_name VARCHAR(100),
+			song_id VARCHAR(25),
+			timestamp VARCHAR(50),
+			playing_type VARCHAR(5),
+			year INT,
+			month INT,
+			day INT,
+			weekday INT,
+			hour INT,
+			minute INT
+			);
+		'''
+
+		cursor.execute(create_table_query)
+		postgre_connection.commit()
+		print(colored("Table 'spotify_tracks' was created (if it not existed already)", "green"))
+	except OperationalError as e:
+		print(colored(f"Could not create 'spotify_tracks' due to exception {e}", "red"))
+	finally:
+		cursor.close()
+
+def write_to_postgres(batch_df, batch_id):
+	url = os.getenv("DB_URL")
+	user = os.getenv("DB_USER")
+	password = os.getenv("DB_PASSWORD")
+
+	if not url or not user or not password:
+		print(colored("Database credentials are missing! Please check your environmental variables!", "red"))
+		return
+
+	if not batch_df:
+		return
+	try:
+		batch_df.write \
+			.format("jdbc") \
+			.option("url", url) \
+			.option("dbtable", "spotify_tracks") \
+			.option("user", user) \
+			.option("password", password) \
+			.option("driver", "org.postgresql.Driver") \
+			.mode("append") \
+			.save()
+		print(colored(f"Batch {batch_id} loaded to PostgreSQL successfully", "green"))
+		return
+	except Exception as e:
+		print(colored(f"Couldn't load data in the database due to exception: {e}", "red"))
+		return
 
 def process_kafka_stream():
 	spark_connection = create_spark_connection()
@@ -183,11 +269,17 @@ def process_kafka_stream():
 	if not enriched_data:
 		return
 	
+	postgre_connection = create_postgre_connection()
+	if not postgre_connection:
+		return
+	
+	create_table(postgre_connection)
+	
 	enriched_data.writeStream \
 		.outputMode("append") \
-		.format("console") \
-		.option("truncate", False) \
-		.start()
+		.foreachBatch(write_to_postgres) \
+		.start() \
+		.awaitTermination()
 
 def send_playback_to_kafka():
 	try:
@@ -209,13 +301,13 @@ def send_playback_to_kafka():
 						producer.send("spotify_tracks", value=parsed_playback)
 						process_kafka_stream()
 					else:
-						print("Skipping duplicate or invalid track")
+						print(colored("Skipping duplicate or invalid track", "cyan"))
 				else:
-					print("No active playback")
-				time.sleep(120)
+					print(colored("No active playback", "cyan"))
+				time.sleep(60)
 			except Exception as e:
-				print(f"An error occured in the playbsck loop: {e}")
+				print(colored(f"An error occured in the playbsck loop: {e}", "red"))
 	except Exception as e:
-		print(f"An error occured while sending playback to Kafka: {e}")
+		print(colored(f"An error occured while sending playback to Kafka: {e}", "red"))
 
 send_playback_to_kafka()
